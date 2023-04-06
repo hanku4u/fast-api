4f1df0acb08db4a6b0ac664d45cee1e36e68b66d
Nick <hemogram4u@yahoo.com>
2023-04-05 22:53:42 -0500

    code cleanup and added requirements.txt

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from routers import (                           # import routers from routers fo
             users,
             address
             )
-from company import companyapis, dependencies   # import companyapis router from company folder
 
 # Creating an instance of the FastAPI application
 app = FastAPI()
@@ -20,10 +19,12 @@ app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(users.router)
 app.include_router(address.router)
-app.include_router(
-    companyapis.router,
-    prefix="/companyapis",
-    tags=["companyapis"],
-    dependencies=[Depends(dependencies.get_token_header)], 
-    responses={418: {"description": "Internal Use Only"}},
-    )
+
+# example of adding prefix, tags, dependencies, and responses to a router
+# app.include_router(
+#     companyapis.router,
+#     prefix="/companyapis",
+#     tags=["companyapis"],
+#     dependencies=[Depends(dependencies.get_token_header)], 
+#     responses={418: {"description": "Internal Use Only"}},
+#     )
