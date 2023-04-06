aa02aaa5557b10d2596851b303e9d9ae31b2d374
Nick <hemogram4u@yahoo.com>
2023-04-05 21:24:48 -0500

    added more routes

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Depends
 import models                                   # import models from models.py
 from database import engine                     # import engine from database.py
-from routers import auth, todos                 # import auth and todos router from routers folder
+from routers import auth, todos, users          # import auth and todos router from routers folder
 from company import companyapis, dependencies   # import companyapis router from company folder
 
 # Creating an instance of the FastAPI application
@@ -10,10 +10,10 @@ app = FastAPI()
 # Creating the database tables based on the models
 models.Base.metadata.create_all(bind=engine)
 
-app.include_router(auth.router)                 # Adding the auth router to the application
-app.include_router(todos.router)                # Adding the todos router to the application
-
-# Adding the companyapis router to the application
+# adding the routers to the application for auth, todos, and users
+app.include_router(auth.router)
+app.include_router(todos.router)
+app.include_router(users.router)
 app.include_router(
     companyapis.router,
     prefix="/companyapis",
