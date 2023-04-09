d91565d1f4852fe03c384a91fb164fd5fd86cd22
Nick <hemogram4u@yahoo.com>
2023-04-09 16:55:21 -0500

    switched to sqlite db

--- database.py
+++ database.py
@@ -3,18 +3,18 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # ===========================sqlite database connection==========================
-# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
 
-# engine = create_engine(
-#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-# )
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
 # ===========================sqlite database connection==========================
 
 
 # ===========================postgresql database connection==========================
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1bigone@localhost:5432/TodoApplicationDatabase"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1bigone@localhost:5432/TodoApplicationDatabase"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
 # ===========================postgresql database connection==========================
 
 
